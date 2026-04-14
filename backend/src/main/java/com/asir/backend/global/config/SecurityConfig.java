0e8a89b8cd634bf5ffe47995bd92dcd36dee127b
inbn3891 <inbn3891@gmail.com>
2026-04-14 22:34:49 +0900

    test booting

--- backend/src/main/java/com/asir/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/asir/backend/global/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
