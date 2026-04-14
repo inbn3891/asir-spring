98a00fea1a227f5a361f6a95c49da41c172954c8
inbn3891 <inbn3891@gmail.com>
2026-04-14 23:34:29 +0900

    b

--- backend/src/main/java/com/asir/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/asir/backend/global/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +26,8 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> 
+    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .headers(headers -> headers.frameOptions(frame -> frame.disable()))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
